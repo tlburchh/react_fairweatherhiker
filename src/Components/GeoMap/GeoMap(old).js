68e7b82e42a48bdd1b845a432ec6a119f8d39ce0
tlburchh <37091749+tlburchh@users.noreply.github.com>
2018-11-07 15:16:04 -0500

    markers are showing!

--- src/Components/GeoMap/GeoMap(old).js
+++ src/Components/GeoMap/GeoMap(old).js
@@ -1,11 +1,11 @@
 import "mapbox-gl/dist/mapbox-gl.css";
 import React, { Component } from "react";
 // import {render} from 'react-dom';
-import MapGL, {Marker} from 'react-map-gl';
+import MapGL, {Marker, NavigationControl, Popup } from 'react-map-gl';
 import Geocoder from "react-map-gl-geocoder";
 import MARKER_STYLE from '../marker-style';
-// import trails from '../trails.json';
-import API from '../../utils/API'
+import trails from '../trails.json';
+import API from '../../utils/API(old)'
 // import Locator from '../Locator/Locator'
 // import ControlPanel from './control-panel';
 // import TrailPin from './TrailPin';
@@ -14,7 +14,12 @@ import API from '../../utils/API'
 
 // Ways to set Mapbox token: https://uber.github.io/react-map-gl/#/Documentation/getting-started/about-mapbox-tokens
 const MAPBOX_TOKEN = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA';
-
+const navStyle = {
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  padding: '10px'
+};
 
 class GeoMap extends Component {
   constructor(props) {
@@ -26,11 +31,10 @@ class GeoMap extends Component {
         latitude: 35.9940329,
         longitude:  -78.898619,
         zoom: 10,
+        trailsData: [],
       },
-      data: [],
-    }
-    console.log('trails')
-    console.log(this.state.data);
+      popupInfo: null
+  }
   }
   mapRef = React.createRef();
 
@@ -41,14 +45,10 @@ class GeoMap extends Component {
   }
 
 //load saved trails
-loadTrails = (latitude, longitude) => {
-  API.searchTrails(latitude, longitude)
-    .then(res => {
-      console.log('SEARCH TRAILS RES ', res.data.trails);
-      
-      // console.log(data.trails);
-      this.setState({ data: res.data.trails })
-    }
+loadTrails = () => {
+  API.searchTrails()
+    .then(res =>
+      this.setState({ trailsData: res.data })
     )
     .catch(err => console.log(err));
 };
@@ -66,27 +66,41 @@ loadTrails = (latitude, longitude) => {
       latitude: {...this.state.viewport.latitude},
       longitude: {...this.state.viewport.longitude},
     });
-    this.loadTrails(this.state.viewport.latitude, this.state.viewport.longitude);
+    console.log(this.state.viewport)
+    this.loadTrails();
   };
    
   //Renders Hiking Marker
   _renderMarker(station, i) {
-    const {name, summary, latitude, longitude} = station;
+    const {name, latitude, longitude} = station;
     return (
       <Marker 
       key={i} 
-      summary={summary}
       longitude={longitude} 
       latitude={latitude}
       captureDrag={false} 
-      captureDoubleClick={true}>
+      captureDoubleClick={false}>
               {/* <TrailPin size={20} onClick={() => this.setState({popupInfo: station})}><span>{name}</span></TrailPin> */}
-      {/* onClick={console.log('clicked')} */}
-        <div className="station" ><span>{name}</span></div>
+
+        <div className="station"><span>{name}</span></div>
       </Marker>
     );
   }
-  
+  //Render popup for hiking marker
+  _renderPopup() {
+    const {popupInfo} = this.state;
+
+    return popupInfo && (
+      <Popup tipSize={5}
+        anchor="top"
+        longitude={popupInfo.longitude}
+        latitude={popupInfo.latitude}
+        onClose={() => this.setState({popupInfo: null})} >
+        {/* <CityInfo info={popupInfo} /> */}
+        <p>CHECK</p>
+      </Popup>
+    );
+  }
 
 
 
@@ -100,12 +114,13 @@ loadTrails = (latitude, longitude) => {
         mapStyle="mapbox://styles/mapbox/streets-v9"
       >
       {/* <Locator /> */}
-        {/* <div className="nav" style={navStyle}>
+        <div className="nav" style={navStyle}>
           <NavigationControl onViewportChange={this._updateViewport} />
-        </div> */}
+        </div>
         {/* <ControlPanel containerComponent={this.props.containerComponent} /> */}
           <style>{MARKER_STYLE}</style>
-        { this.state.data.map(this._renderMarker) }
+        { this.state.viewport.trailsData.map(this._renderMarker) }
+        {this._renderPopup()}
   
         <Geocoder mapRef={this.mapRef} onViewportChange={this._onViewportChange} mapboxApiAccessToken={MAPBOX_TOKEN} />
       </MapGL>
--- src/utils/API.js
+++ src/utils/API.js
@@ -2,16 +2,15 @@ import axios from "axios";
 
 
 //receive trails from Hiking API
-const PROXY = 'https://urlreq.appspot.com/req?method=GET&url='
-const BASEURL = 'https://www.hikingproject.com/data/get-trails?';
-const latRef =  'lat=';
-const longRef = '&lon=';
-const results = '&maxResults=50';
-const KEY = '&key=200310958-80eadbd0eda211e9f1bec2cca75b17cb';
-// console.log(searchTrails)
-// const APIKEY = REACT_APP_TRAILS
+// const PROXY = 'urlreq.appspot.com/req?method=GET&url='
+// const BASEURL = 'https://www.hikingproject.com/data/get-trails?';
+// const latRef =  'lat=';
+// const longRef = '&lon=';
+// const results = '&maxResults=50';
+// const KEY = '&key=200310958-80eadbd0eda211e9f1bec2cca75b17cb';
+
           export default {
   searchTrails: function(latitude, longitude) {
-    return axios.get(PROXY + BASEURL + latRef + latitude + longRef + longitude + results + KEY );
+    return axios.get('http://urlreq.appspot.com/req?method=GET&url=https%3A%2F%2Fwww.hikingproject.com%2Fdata%2Fget-trails%3Flat%3D' + latitude + '%26lon%3D' + longitude + '%26maxResults%3D75%26maxDistance%3D15%26key%3D200310958-80eadbd0eda211e9f1bec2cca75b17cb');
   },
 };
